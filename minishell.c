71e8532a701d35033b10592261c5f7b97a43f469
Baptiste Godeau <baptiste.godeau@epitech.eu>
2019-12-02 17:30:30 +0100

    UwU

--- minishell.c
+++ minishell.c
@@ -58,7 +58,7 @@ int minishell_stand_imput(int fd, env_struct *env)
     exit_codes[0] = 0;
     exit_codes[1] = 0;
     while (read_var > 0 && exit_codes[0] == 0) {
-        my_putstr("#> ");
+        my_putstr("<(UwU)> ");
         reset_buffer(buffer, read_var);
         read_var = read(fd, buffer, 4096);
         if(read_var > 1) {
