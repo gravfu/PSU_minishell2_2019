c72d10e9f4b68cea17ccf8a40c7369ad2ca5ebed
Vincent DOUROUX <vincent.douroux@epitech.eu>
2019-12-12 15:28:04 +0100

    Adding isatty to show prompt or not

--- minishell.c
+++ minishell.c
@@ -52,21 +52,19 @@ char **read_commands(char *buffer, int readsize)
 
 int minishell_stand_imput(int fd, char **env)
 {
-    int read_var = 1;
+    int read_var = 4096;
     int *exit_codes = malloc(sizeof(int)*2);
     char *buffer = malloc(sizeof(char) * 4097);
     char **argv;
     size_t size = 4096;
 
-    buffer[4096] = '\0';
     exit_codes[0] = 0;
     exit_codes[1] = 0;
     while (read_var > 0 && exit_codes[0] == 0) {
-        my_putstr("=(^-^)= ");
+        if (isatty(0))
+            my_putstr("=(^-^)= ");
         reset_buffer(buffer, read_var);
         read_var = getline(&buffer, &size, stdin);
-        //read_var = read(fd, buffer, 4096);
-        //write(1, buffer, read_var);
         if (read_var > 1) {
             argv = read_commands(buffer, read_var);
             exit_codes = minishell_command(argv, read_var, env, exit_codes[1]);
